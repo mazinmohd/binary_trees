9c4836acef2206a08c735ad33f6259068ff903f1
mazinmohd <mazenomermazen2002@gmail.com>
2024-01-31 23:21:08 +0300

    task num 12

---
+++ 12-binary_tree_leaves.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+/**
+  * binary_tree_leaves - count the leaves
+  * @tree: a pointer to the root node of the tree
+  * Return: if is NULL return 0
+ */
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree)
+	{
+		leaves += (!tree->left && !tree->right) ? 1 : 0;
+		leaves += binary_tree_leaves(tree->left);
+		leaves += binary_tree_leaves(tree->right);
+	}
+	return (leaves);
+}
