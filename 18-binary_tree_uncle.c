6b30b68dc57f9314394de7a9e91abbc9802dabfe
mazinmohd <mazenomermazen2002@gmail.com>
2024-02-01 12:36:59 +0300

    last task on mandatry

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,36 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_sibling - finds the sibling of a node
+ * @node: pointer to the node to find the sibling
+ * Return: sibling or NULL
+ */
+binary_tree_t *binary_tree_sibling(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (!node || !node->parent)
+		return (NULL);
+
+	parent = node->parent;
+	if (!parent->left || !parent->right)
+		return (NULL);
+
+	if (parent->left != node)
+		return (parent->left);
+	else
+		return (parent->right);
+	return (NULL);
+}
+/**
+  * binary_tree_uncle - finds the uncle of node
+  * @node: a pointer to the node to find the uncle
+  * Return: pinter to the uncle or NULL
+ */
+
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	if (node == NULL)
+		return (NULL);
+	return (binary_tree_sibling(node->parent));
+}
